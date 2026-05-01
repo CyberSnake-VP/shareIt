e86b05c3f8aaf5e3cf4343b33396242875058ec4
Vadim <zigmundfreid89@yandex.ru>
2026-05-01 19:11:37 +0300

    Добавлена таблица bookings

---
+++ src/main/java/example/booking/Booking.java
@@ -0,0 +1,5 @@
+package example.booking;
+
+public class Booking {
+
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,51 @@
-CREATE TABLE IF NOT EXISTS users (
-    id BIGINT GENERATED ALWAYS AS IDENTITY,
-    name varchar(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS users
+(
+    id    BIGINT GENERATED ALWAYS AS IDENTITY,
+    name  varchar(255) NOT NULL,
     email varchar(320) NOT NULL,
     CONSTRAINT pk_users PRIMARY KEY (id),
     CONSTRAINT unique_users_email UNIQUE (email)
 );
 
-CREATE TABLE IF NOT EXISTS items (
-  id BIGINT GENERATED ALWAYS AS IDENTITY,
-  name varchar(255) NOT NULL,
-  description varchar(1000) NOT NULL,
-  available boolean NOT NULL,
-  owner_id BIGINT NOT NULL ,
-  CONSTRAINT pk_items PRIMARY KEY (id),
-  CONSTRAINT fk_items_users FOREIGN KEY (owner_id) REFERENCES users (id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS items
+(
+    id          BIGINT GENERATED ALWAYS AS IDENTITY,
+    name        varchar(255)  NOT NULL,
+    description varchar(1000) NOT NULL,
+    available   boolean       NOT NULL,
+    owner_id    BIGINT        NOT NULL,
+    CONSTRAINT pk_items PRIMARY KEY (id),
+    CONSTRAINT fk_items_users FOREIGN KEY (owner_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS bookings
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    start_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    end_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    item_id BIGINT NOT NULL,
+    booker_id BIGINT NOT NULL,
+    status varchar(100) NOT NULL,
+    CONSTRAINT pk_key PRIMARY KEY (id),
+    CONSTRAINT fk_bookings_items FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE CASCADE,
+    CONSTRAINT fk_bookings_users FOREIGN KEY (booker_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_bookings_item_id ON bookings (item_id);
+
+CREATE INDEX IF NOT EXISTS idx_bookings_booker_id ON bookings (booker_id);
+
+CREATE INDEX IF NOT EXISTS idx_bookings_status ON bookings (status);
+
+-- Частый запрос: бронирования пользователя по статусу
+CREATE INDEX idx_bookings_booker_status ON bookings (booker_id, status);
+
+-- Частый запрос: пересечение дат для предмета
+CREATE INDEX idx_bookings_item_dates ON bookings (item_id, start_date, end_date);
+
+-- Частый запрос: владелец вещи + статус
+CREATE INDEX idx_bookings_owner_status ON bookings (item_id, status);
+
 
 -- ✅ Обязательно (частые запросы по владельцу)
 CREATE INDEX IF NOT EXISTS idx_items_owner_id ON items (owner_id);
@@ -24,3 +55,4 @@ CREATE INDEX IF NOT EXISTS idx_items_available ON items (available);
 
 -- Для поиска по названию
 CREATE INDEX IF NOT EXISTS idx_items_name ON items (name);
+
